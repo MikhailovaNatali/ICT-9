6a73263cc80c4638f00034af64d78422a54cf0ff
MikhailovaNatalia <mikhaylova.elena32@gmail.com>
2026-01-23 16:11:51 +0300

    correct task6

--- 9/3/2026-01-20/task6.py
+++ 9/3/2026-01-20/task6.py
@@ -1,4 +1,10 @@
+import string
+
 a = input()
 a = a.split(" ")
+d = {}
+for s in a:
+    s = s.strip(string.punctuation).lower()
+
 d = {a[i]: i + 1 for i in range(0, len(a))}
-print(d[a[len(a) -  1]])
+print(len(d))
