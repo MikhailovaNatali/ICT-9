fa4d4a7b65db942a7cd6820d3883b7a1158ff205
MikhailovaNatalia <mikhaylova.elena32@gmail.com>
2026-02-16 14:52:03 +0300

    edit

--- 9/3/2026-02-16/Ковёр_Серпинского.py
+++ 9/3/2026-02-16/Ковёр_Серпинского.py
@@ -1,5 +1,7 @@
+import random
 from turtle import *
-speed(1000)
+speed(10000)
+teleport(-200, -50)
 x = 5
 y = 10
 st = "FLGLG"
@@ -18,6 +20,17 @@ for a in range(x):
 print(st)
 for i in st:
     if(i == "F" or i == "G"):
+        a = random.randint(1, 5)
+        if a == 1: 
+            color("dark blue")
+        if a == 2: 
+            color("light sky blue")
+        if a == 3: 
+            color("steel blue")
+        if a == 4: 
+            color("midnight blue")
+        if a == 5: 
+            color("royal blue")
         forward(y)
     if(i == "L"):
         left(120)
