175cfbbb5e528e2a894c29712605987d8b72e16a
MikhailovaNatalia <mikhaylova.elena32@gmail.com>
2026-02-16 14:45:42 +0300

    edit

--- 9/3/2026-02-16/_2026_02_15_Кривая_дракона.py
+++ 9/3/2026-02-16/_2026_02_15_Кривая_дракона.py
@@ -1,31 +1,34 @@
+import random
 from turtle import *
 tracer(0)
-#teleport(-250, 130)
 x = 12
 y = 2
 st = "FX"
 for a in range(x):
     i = 0
-    print("asdfghjkl")
     while i < len(st):
         if(st[i] == "X"):
             st = st[:i] + "XRYFR" + st[i+1:]
             print(st)
             i = i + 5
         if(i < len(st)):
-            print("ertyuio")
             if (st[i] == "Y"):
-                print("zxcvbnm")
                 st = st[:i] + "LFXLY" + st[i+1:]
                 i = i + 5
             else:
-                print("qwerfghyujkl")
                 i = i + 1
         
         
 print(st)
 for i in st:
     if(i == "F"):
+        a = random.randint(1, 3)
+        if a == 1: 
+            color("dark blue")
+        if a == 2: 
+            color("light sky blue")
+        if a == 3: 
+            color("steel blue")
         forward(y)
     if(i == "L"):
         left(90)
