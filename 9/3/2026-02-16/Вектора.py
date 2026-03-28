becd6453a8d25b468e3a01c3ee3a896280e6ff16
MikhailovaNatalia <mikhaylova.elena32@gmail.com>
2026-03-28 08:26:13 +0300

    hw

--- 9/3/2026-02-16/Вектора.py
+++ 9/3/2026-02-16/Вектора.py
@@ -8,10 +8,25 @@ def angle(n, v):
     return  math.degrees(math.acos((n@v)/(np.linalg.norm(n)*np.linalg.norm(v))))
 print(angle(b, p))
 a = [b, p, c, d]
+print("####")
 for i in a:
     for j in a:
         if(angle(i, j) == 90):
-            print("###")
             print(i)
             print(j)
-            
+print("####")
+def projection(n, v):
+    return  ((n@v)/(np.linalg.norm(v)))
+print(projection(c, b))
+
+#Ортогональный базис :
+e1 = np.array([1, 0])
+e2 = np.array([0, 1])
+
+print("#####")
+def Decomposition(r):
+    x1 = (r@e1)/((np.linalg.norm(e1))**2)
+    x2 = (r@e2)/((np.linalg.norm(e2))**2)
+    print(x1)
+    print(x2)
+Decomposition(c)
